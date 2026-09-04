13afac5996607608b90acfefd6bc58964653c3c0
slayers103-web <slayers103@gmail.com>
2026-09-04 09:17:45 +0900

    v2.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.monthlytodolist"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "2.8"
+        versionCode = 7
+        versionName = "2.9"
     }
 
     compileOptions {
